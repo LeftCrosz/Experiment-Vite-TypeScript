65e7dfc3a4e13ca2369a11a35af276f18cc1b4f2
Wai Lam <leftcross112@gmail.com>
2026-07-02 19:30:34 +0800

    Simple FAQ with TypeScript

---
+++ my-project/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Home from './components/home.vue'
+</script>
+
+<template>
+  <home />
+</template>
---
+++ my-project/src/components/home.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+function toggle() {
+  if(isOpen.value) {
+    isOpen.value = false;
+  } else {
+    isOpen.value = true;
+  }
+}
+</script>
+
+
+<template>
+  <h1 class="text-green-500 text-[60px] font-bold text-center">FAQ</h1>
+  
+  <section>
+    <div class="border rounded-sm ml-10 mr-10 p-2 bg-gray-700 font-bold text-white">
+      <p v-on:click="toggle">Question 1</p>
+      <p id="ans-1" class="overflow-hidden transition-all duration-200 ease-in-out" :class="{ 'max-h-40' : isOpen, 'max-h-0': !isOpen }">Answer 1</p>
+    </div>
+  </section>
+</template>
